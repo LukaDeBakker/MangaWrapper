ff498dc2cf497216d34509ed61e26c2092991faa
LukaDeBakker <lukadebakker5@gmail.com>
2022-07-09 19:48:50 +0200

    :memo: Code comments.
    Added code comments to authentication.

--- scraper/mangaDex/auth_route.js
+++ scraper/mangaDex/auth_route.js
@@ -3,14 +3,18 @@ const fetch = require('node-fetch');
 var mangaDexUrl = 'https://api.mangadex.org'
 
 module.exports = async function(req, res, next) {
+    // Take data out of request body.
     const username = req.body.username
     const password = req.body.password
 
+    // Create JSON payload.
     const payload = {
         "username": username,
         "password": password
     }
 
+    // Call URL and retrieve data.
+    // Then return this data.
     await fetch(`${mangaDexUrl}/auth/login`, 
     {
         headers: {
